86cf81fc3dfe67c024a0d6cfbc7ad9bd2a079c02
ikoote1 <ikootepreim@gmail.com>
2023-03-31 02:32:07 +0300

    Working on setups

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Routes,Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>IKOOTE</h1>
+      <Routes>
+        <Route path='/' element={<Books />}/>
+        <Route path='/categories' element={<Categories />}/>
+      </Routes>
      
     </div>
   );
