fbd49c773cc2b9667dd139fceae072f7a3c2e3b4
ikoote1 <ikootepreim@gmail.com>
2023-03-31 02:55:58 +0300

    Working on setups

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Routes,Route } from 'react-router-dom';
 import './App.css';
 import Books from "./components/Books"
 import Categories from "./components/Categories"
+import navigation from './components/navigation';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
+      <navigation />
+      <Routes>   
         <Route path='/' element={<Books />}/>
         <Route path='/categories' element={<Categories />}/>
       </Routes>
