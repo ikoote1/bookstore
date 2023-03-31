ddd4c2c91f217119da1e77064a9acb9aa6bb4788
ikoote1 <ikootepreim@gmail.com>
2023-03-31 16:54:00 +0300

    Updated the bookstore code

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,27 +1,28 @@
+import { Form } from 'react-router-dom';
+
 const Books = () => (
   <div>
     <div className="books">
       <div className="book">
-        <h2>Ikoote</h2>
+        <div> <h2>Ikoote</h2>
         <h3>Ikoote Rasuli</h3>
+        </div>
+        <button type='button'>Delete</button>
       </div>
       <div className="book">
-        <h2>Ikoote</h2>
+        <div> <h2>Ikoote</h2>
         <h3>Ikoote Rasuli</h3>
+        </div>
+        <button type='button'>Delete</button>
       </div>
       <div className="book">
-        <h2>Ikoote</h2>
+        <div> <h2>Ikoote</h2>
         <h3>Ikoote Rasuli</h3>
+        </div>
+        <button type='button'>Delete</button>
       </div>
     </div>
-    <div className="form">
-      <h1>ADD NEW BOOK</h1>
-      <form>
-        <input placeholder="Book name.." />
-        <input placeholder="Name of the author.." />
-        <button type="submit">Addbook</button>
-      </form>
-    </div>
+    <Form />
   </div>
 );
 
---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+const Form = () => (
+  <div className="form">
+    <h1>ADD NEW BOOK</h1>
+    <form>
+      <input placeholder="Book name.." />
+      <input placeholder="Name of the author.." />
+      <button type="submit">Addbook</button>
+    </form>
+  </div>
+);
+
+export default Form;
