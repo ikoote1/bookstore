3d021c63af898caa17dc3022ff5273709995c9b5
ikoote1 <ikootepreim@gmail.com>
2023-03-31 16:02:49 +0300

    Updated the bookstore code

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,27 +1,27 @@
-const Books = () => {
-  return(
-    <div>
-      <div className="books">
-    <div className="book">
-      <h2>Ikoote</h2>
-      <h3>Ikoote Rasuli</h3>
+const Books = () => (
+  <div>
+    <div className="books">
+      <div className="book">
+        <h2>Ikoote</h2>
+        <h3>Ikoote Rasuli</h3>
+      </div>
+      <div className="book">
+        <h2>Ikoote</h2>
+        <h3>Ikoote Rasuli</h3>
+      </div>
+      <div className="book">
+        <h2>Ikoote</h2>
+        <h3>Ikoote Rasuli</h3>
+      </div>
     </div>
-    <div className="book">
-      <h2>Ikoote</h2>
-      <h3>Ikoote Rasuli</h3>
-    </div>
-    <div className="book">
-      <h2>Ikoote</h2>
-      <h3>Ikoote Rasuli</h3>
+    <div className="form">
+      <form>
+        <input />
+        <input />
+        <button type="submit" />
+      </form>
     </div>
   </div>
-  <div className="form">
-    <form>
-
-    </form>
-  </div>
-    </div>
-  )
-  };
+);
 
 export default Books;
