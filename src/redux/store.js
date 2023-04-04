f1bdc719878cd732f2e4e19f66778c9a7499fa38
ikoote1 <ikootepreim@gmail.com>
2023-04-04 11:06:46 +0300

    adding code

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import categoryReducer from './categories/categoriesSlice';
+import bookReducer from './books/booksSlice';
 
 const store = configureStore({
   reducer: {
     category: categoryReducer,
+    book: bookReducer,
   },
 });
 
