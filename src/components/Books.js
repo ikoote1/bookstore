4c38130e2332e5aaffa756ea0d51067e78fc2138
ikoote1 <ikootepreim@gmail.com>
2023-04-05 02:19:15 +0300

    working on functionarity

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,58 +1,37 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { removeBook, addBook } from '../redux/books/booksSlice';
-// import AddBook from './AddBook';
+import { removeBook } from '../redux/books/booksSlice';
+import AddBook from './AddBook';
 
 const Books = () => {
   const dispatch = useDispatch();
   const { bookItems } = useSelector((state) => state.book);
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch(addBook({ title, author }));
-    setTitle('');
-    setAuthor('');
-  };
   return (
     <div>
-      <div className="books">
-        {bookItems.map(
-          (item) => (
-            <div className="book" key={item.id}>
-              <div>
-                <h2>{item.title}</h2>
-                <h2>
-                  {' '}
-                  by
-                  {' '}
-                </h2>
-                <h2>
-                  {item.author}
-                </h2>
+      <div>
+        <div className="books">
+          {bookItems.map(
+            (item) => (
+              <div className="book" key={item.id}>
+                <div>
+                  <h2>{item.title}</h2>
+                  <h2>
+                    {' '}
+                    by
+                    {' '}
+                  </h2>
+                  <h2>
+                    {item.author}
+                  </h2>
+                </div>
+                <button type="button" onClick={() => { dispatch(removeBook(item.id)); }}>Delete</button>
               </div>
-              <button type="button" onClick={() => { dispatch(removeBook(item.id)); }}>Delete</button>
-            </div>
-          ),
-        )}
+            ),
+          )}
+          <div className="chart" />
+        </div>
       </div>
-      <div className="form">
-      <h1>ADD NEW BOOK</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          placeholder="Book name.."
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <input
-          placeholder="Name of the author.."
-          value={author}
-          onChange={(e) => setAuthor(e.target.value)}
-        />
-        <button type="submit">Add Book</button>
-      </form>
-    </div>
+      <AddBook />
     </div>
   );
 };
