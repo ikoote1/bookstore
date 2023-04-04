b1471dff85c4a1b7b6e9eb19ec975edb2a16bd74
ikoote1 <ikootepreim@gmail.com>
2023-04-04 22:35:32 +0300

    working on functionarity

--- src/components/book.js
+++ src/components/book.js
@@ -13,20 +13,6 @@ const BookSet = () => {
         </div>
         <button type="button" onClick={() => { dispatch(removeBook()); }}>Delete</button>
       </div>
-      <div className="book" id="2">
-        <div>
-          <h2>Ikoote</h2>
-          <h2> by   Ikoote Rasuli</h2>
-        </div>
-        <button type="button" onClick={() => { dispatch(removeBook()); }}>Delete</button>
-      </div>
-      <div className="book" id="3">
-        <div>
-          <h2>Ikoote</h2>
-          <h2> by   Ikoote Rasuli</h2>
-        </div>
-        <button type="button" onClick={() => { dispatch(removeBook()); }}>Delete</button>
-      </div>
     </div>
   );
 };
