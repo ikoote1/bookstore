abeabde0392d36b21cf56b0a90d5afd3495381e3
ikoote1 <ikootepreim@gmail.com>
2023-04-05 01:56:46 +0300

    working on functionarity

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { removeBook } from '../redux/books/booksSlice';
-import AddBook from './AddBook';
+import { removeBook, addBook } from '../redux/books/booksSlice';
+// import AddBook from './AddBook';
 
 const Books = () => {
   const dispatch = useDispatch();
   const { bookItems } = useSelector((state) => state.book);
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addBook({ title, author }));
+    setTitle('');
+    setAuthor('');
+  };
   return (
     <div>
       <div className="books">
@@ -28,7 +37,22 @@ const Books = () => {
           ),
         )}
       </div>
-      <AddBook />
+      <div className="form">
+      <h1>ADD NEW BOOK</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Book name.."
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          placeholder="Name of the author.."
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <button type="submit">Add Book</button>
+      </form>
+    </div>
     </div>
   );
 };
