f2a63aadf728b46e53323aead0bd826223a3c086
ikoote1 <ikootepreim@gmail.com>
2023-04-06 16:02:17 +0300

    posting  data

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import ( v4 as uuidv4 ) from 'uuid';
 import { addBook, postBook } from '../redux/books/booksSlice';
+import { v4 as uuidv4 } from 'uuid';
 
 const AddBook = () => {
   const dispatch = useDispatch();
